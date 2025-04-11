24a1bc620a7908ead25e9198eb793c30b0f0af31
{B-Lubanga} <{Lubangabenjamin@gmail.com}>
2025-04-11 18:16:00 +0300

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/B-Lubanga/SA.01-Week-1-Code-Challenge-Expense-Tracker.git
+// git push -u origin main
